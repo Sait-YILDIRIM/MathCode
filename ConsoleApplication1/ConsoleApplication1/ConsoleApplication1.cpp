d828fcf992905866375c3e690882afbce7324ff9
Sait_YILDIRIM <saitf.yildirim@gmail.com>
2023-11-23 23:01:33 +0300

    mathcode
    Rakamları toplamı 20 den büyük ve rakamları çarpımı 200 ile 500 arasında olan 4 basamaklı sayılar
    (Write a program that lists all the 4-digit numbers whose sum of the digits is greater than 20 and product
    of the digits is between 50 and 200.)

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+bool ValidNumber(int number) {
+    // sayinin kosullarini kontrol ettiğimiz kisim
+    int sumOfDigits = 0;
+    int productOfDigits = 1;
+
+    // sayilari olusturma kismi
+    int tempNumber = number;
+    while (tempNumber > 0) {
+        int digit = tempNumber % 10;
+        sumOfDigits += digit;
+        productOfDigits *= digit;
+        tempNumber /= 10;
+    }
+
+    return sumOfDigits > 20 && productOfDigits >= 50 && productOfDigits <= 200;
+}
+
+void findNumbers() {
+    // sayilari bulma ve yazdirma 
+    std::cout << "4-digit numbers with sum of digits > 20 and product of digits between 50 and 200:\n";
+
+    for (int number = 1000; number < 10000; ++number) {
+        if (ValidNumber(number)) {
+            std::cout << number << std::endl;
+        }
+    }
+}
+
+int main() {
+    findNumbers();
+
+    return 0;
+}
